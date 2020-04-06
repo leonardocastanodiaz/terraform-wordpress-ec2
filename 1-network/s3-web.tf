479bec07b28648a5eca9ad574e88e35607678a3b
Leonardo Castano <leonardo.castano@gmail.com>
2020-04-06 20:29:31 +0100

    HTML1

--- 1-network/s3-web.tf
+++ 1-network/s3-web.tf
@@ -25,7 +25,6 @@ resource "aws_s3_bucket_object" "upload-index" {
   source       = "html"
   content_type = "text/html"
   acl          = "public-read"
-  etag         = "${md5(file("html"))}"
 }
 
 
